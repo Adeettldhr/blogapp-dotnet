a62e0ee38377f0f37ac0a00dc6b1663ecbfa08ec
Adeet Tuladhar <adeettuldhr5@gmail.com>
2025-09-27 13:05:58 -0700

    inital commit

--- obj/Debug/net9.0/BlogApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/BlogApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlogApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94fe28645db60e179071608a36019dd9a6c05c92")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0ae0641b2d27a545496898200323302f9d42167")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlogApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlogApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
